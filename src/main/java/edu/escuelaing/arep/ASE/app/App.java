81ef96a55a2e9239b356a2e8562c45457bbe130a
Natalia Palacios <natalia.palacios-r@mail.escuelaing.edu.co>
2019-08-22 18:12:26 -0500

    Avanzando lab

--- src/main/java/edu/escuelaing/arep/ASE/app/App.java
+++ src/main/java/edu/escuelaing/arep/ASE/app/App.java
@@ -1,13 +1,17 @@
 package edu.escuelaing.arep.ASE.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import static spark.Spark.*;
+
+public class App {
+	
+    public static void main(String[] args) {        
+    	get("/hello", (req, res) -> "Hello Heroku");    
+    }
+    
+    static int getPort() {        
+    	if (System.getenv("PORT") != null) {            
+    		return Integer.parseInt(System.getenv("PORT"));        
+    	}        
+    	return 4567;
     }
 }

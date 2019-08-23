1bd7d664c4e4bfcb1a4d5a91444f0a8b54763c4e
Natalia Palacios <natalia.palacios-r@mail.escuelaing.edu.co>
2019-08-23 00:13:03 -0500

    lab 2

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java $JAVA_OPTS -cp target/classes:target/dependency/*edu.escuelaing.arep.ASE.App
+web: java $JAVA_OPTS -cp target/classes:target/dependency/*edu.escuelaing.arep.ASE.app.App
--- src/main/java/edu/escuelaing/arep/ASE/app/App.java
+++ src/main/java/edu/escuelaing/arep/ASE/app/App.java
@@ -2,6 +2,9 @@ package edu.escuelaing.arep.ASE.app;
 
 import static spark.Spark.*;
 
+import spark.Request;
+import spark.Response;
+
 /**
  * 
  * @author Natalia Palacios
@@ -17,20 +20,72 @@ public class App {
     	
     	//setPort(4567);
         port(getPort());
-        //get("/inputdata", (req, res) -> inputDataPage(req, res));
-        //get("/results", (req, res) -> resultsPage(req, res));
+        get("/introDatos", (req, res) -> introducirDatos(req, res));
+        get("/operaciones", (req, res) -> operaciones(req, res));
     }
     
+	private static String introducirDatos(Request req, Response res) {
+        String pageContent
+                = "<!DOCTYPE html>"
+                + "<html>"
+                + "<body>"
+                + "<h2>MEDIA Y DESVIACIÓN ESTÁNDAR</h2>"
+                + "<h3>Escriba los números separados por un espacio</h3>"
+                + "<form action=\"/results\">"
+                + "  Columna 1:<br>"
+                + "  <input type=\"text\" name=\"columna1\" value=\"\" required>"
+                + "  <br>"
+                + "  Columna 2):<br>"
+                + "  <input type=\"text\" name=\"columna2\" value=\"\" required"
+                + "  <br><br>"
+                + "  <input type=\"submit\" value=\"Submit\">"
+                + "</form>"
+                + "</body>"
+                + "</html>";
+        
+        return pageContent;
+    }
+
+    private static String operaciones(Request req, Response res) {
+        String pageContent;
+        String[] c1 = req.queryParams("columna1").split(" ");
+        String[] c2 = req.queryParams("columna2").split(" ");
+        double[] columna1 = leer(c1);
+        double[] columna2 = leer(c2);
+        
+        double media1 = media(columna1);
+        double media2 = media(columna2);
+        
+        double desviacion1 = desviacion(columna1, media1);
+        double desviacion2 = desviacion(columna2, media2);
+        
+        if (columna1.length == columna2.length){
+            pageContent
+            = "<!DOCTYPE html>"
+            + "<html>"
+            + "<body>"
+            + "<h2>Los resultados son:</h2>"
+            + "<h3 style=\"color:red;\">Columna 1:</h3>"
+            + "<h4>"+"MEDIA:"+"  "+String.format("%.2f", media1)+"</h4>"
+            + "<h4>"+"DESVIACIÓN ESTANDAR:"+"  "+String.format("%.2f", desviacion1)+"</h4>"
+            + "<h3 style=\"color:red;\">Columna 2:</h3>"
+            + "<h4>"+"MEDIA:"+"  "+String.format("%.2f", media2)+"</h4>"
+            + "<h4>"+"DESVIACIÓN ESTANDAR:"+"  "+String.format("%.2f", desviacion2)+"</h4>";
+        }else{
+            pageContent="error: columna 1 y columna 2 deben contener la misma cantidad de datos";
+        }
+        return pageContent;
+    }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    public static double[] leer (String leida[]) {
+    	int size = leida.length;
+    	double[] lista = new double[size];
+
+        for ( int i = 0; i < size; i++ ) 
+          lista[i] = Double.parseDouble(leida[i]);
+
+        return lista;
+    }
     
     /**
      * Calcula la media de una lista de numeros.
